63992e779ec33ed313375952c2bbd67eee979735
zjl <1552129096>
2018-07-24 20:19:07 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'bot monitor',
+      UserId: '5a52790f40a1780e52659233',
+      access_token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMzYyMTY4NzkwOCIsImV4cCI6MTUzMTc1MjcyMDg1NX0.UkPCVF93M7uUKs9_7sIYKxKcpj3CIPZFPRy6HhZxnsc'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
